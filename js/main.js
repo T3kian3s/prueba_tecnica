0a432688df26e31da7b004aa6ff3be51500391b1
Olises <uramirez4@ucol.mx>
2025-04-15 13:40:34 -0600

    cambios github pages

--- js/main.js
+++ js/main.js
@@ -90,5 +90,5 @@ function agregarTarea() {
 
 function cerrarSesion() {
   localStorage.removeItem("usuarioActivo");
-  window.location.href = "index.html";
+  window.location.href = "/index.html";
 }
